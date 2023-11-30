b6622f8dbd140bea03029a4608db2ecef25e087a
Николаев Илья Кузьмич <inikolaev@alfabank.ru>
2023-11-30 15:53:28 +0400

    Added LinkedList and Tests

---
+++ src/Main/DataStructures/LinkedList.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+
+namespace Main.DataStructures
+{
+    public class LinkedList<T> : IEnumerable<T>
+    {
+        private Node<T>? _head;
+        private Node<T>? _tail;
+        private int _count;
+
+        public void Add(T value)
+        {
+            // Создаем новый элемент, который будем добавлять
+            var node = new Node<T>(value);
+
+            // Если список пустой, то назначаем головному узлу - созданный узел
+            if (_head == null)
+                _head = node;
+            // Иначе присваем следующему элементу конечного
+            else
+                _tail!.Next = node;
+
+            // Обновляем конечный элемент - он теперь указывает но добавленный узел
+            _tail = node;
+            _count++;
+        }
+
+        public bool Remove(T value)
+        {
+            Node<T>? current = _head;
+            Node<T>? previous = null;
+
+            while (current != null && current.Value != null)
+            {
+                if (current.Value.Equals(value))
+                {
+                    // Если узел в середине или конце
+                    if (previous != null)
+                    {
+                        // Меняем ссылку предыдущего узла на следующий текущего
+                        // (prev) -> (current) -> (current.Next)
+                        //               X
+                        previous.Next = current.Next;
+
+                        // Если узел последний, то меняем _tail
+                        if (current.Next == null)
+                            _tail = previous;
+                    }
+                    // Удаляется узел в начале
+                    else
+                    {
+                        _head = _head?.Next;
+
+                        if (_head == null)
+                            _tail = null;
+                    }
+
+                    _count--;
+                    return true;
+                }
+                previous = current;
+                current = current.Next;
+            }
+
+            return false;
+        }
+
+        public int Count => _count;
+        public bool IsEmpty => _count == 0;
+
+        public void Clear()
+        {
+            _head = null;
+            _tail = null;
+            _count = 0;
+        }
+
+        // содержит ли список элемент
+        public bool Contains(T value)
+        {
+            Node<T>? current = _head;
+            while (current != null && current.Value != null)
+            {
+                if (current.Value.Equals(value))
+                    return true;
+                current = current.Next;
+            }
+            return false;
+        }
+
+        // добвление в начало
+        public void AppendFirst(T data)
+        {
+            Node<T> node = new Node<T>(data);
+            node.Next = _head;
+            _head = node;
+            if (_count == 0)
+                _tail = _head;
+            _count++;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            var current = _head;
+            while (current != null)
+            {
+                yield return current.Value;
+                current = current.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable<T>)this).GetEnumerator();
+        }
+    }
+}
---
+++ tests/MainTest/DataStructures/LinkedListTests.cs
@@ -0,0 +1,68 @@
+﻿using Main.DataStructures;
+using Xunit;
+
+namespace MainTest.DataStructures
+{
+    public class LinkedListTests
+    {
+        [Theory]
+        [InlineData(10)]
+        public void AddItem_Count_ExpectCountEqual(int itemsCount)
+        {
+            // arrange
+            var linkedList = new LinkedList<int>();
+            
+            // act
+            for(int i = 0; i < itemsCount; i++) 
+            {  
+                linkedList.Add(i); 
+            }
+
+            // assert
+            Assert.Equal(itemsCount, linkedList.Count);
+        }
+
+        [Fact]
+        public void RemoveItem_Count_ExpectCountDecrease()
+        {
+            // arrange 
+            var linkedList = new LinkedList<int>();
+            linkedList.Add(0);
+            linkedList.Add(1);
+            linkedList.Add(2);
+
+            // act
+            linkedList.Remove(0);
+
+            Assert.Equal(2, linkedList.Count);
+        }
+
+        [Fact]
+        public void RemoveItem_Contains_ExpectNotContains()
+        {
+            // arrange 
+            var linkedList = new LinkedList<int>();
+            linkedList.Add(0);
+            linkedList.Add(1);
+            linkedList.Add(2);
+
+            // act
+            linkedList.Remove(0);
+
+            Assert.False(linkedList.Contains(0));
+        }
+
+        [Fact]
+        public void ClearList_Clear_CountEqualsZero()
+        {
+            var linkedList = new LinkedList<int>();
+            linkedList.Add(1);
+            linkedList.Add(2);
+            linkedList.Add(3);
+
+            linkedList.Clear();
+
+            Assert.Equal(0, linkedList.Count);
+        }
+    }
+}
